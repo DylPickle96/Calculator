6199db54af1640a20f2b9a3d06969c4fb037f84c
Dylan Smith <dylan_smartguy@hotmail.com>
2017-11-02 18:11:16 -0700

    started project, added components: calc, square. App is the start point

---
+++ src/components/Calculator.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Square from './Square';
+
+class Calculator extends Component {
+
+	constructor (props) {
+		super(props);
+	}
+
+	render () {
+		return (
+			<Square />
+		);
+	}
+}
+
+export default Calculator;
---
+++ src/components/Square.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const styles = {
+	width: '45px',
+	height: '45px',
+	backgroundColor: 'rgb(233, 151, 72)',
+	border: '2px rgb(142, 142, 142) solid'
+}
+
+const Square = () => {
+	return (
+		<div style={styles}></div>
+	);
+};
+
+export default Square;
