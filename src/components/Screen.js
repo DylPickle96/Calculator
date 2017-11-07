9ad420ebe121fff4d118994a39e0d71ec39305d3
Dylan Smith <dylan_smartguy@hotmail.com>
2017-11-06 17:57:19 -0800

    loaded a blank inanimate screen with it's correct dimensions

---
+++ src/components/Screen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Screen = () => {
+
+	const styles = {
+		backgroundColor: 'rgb(153, 153, 153)',
+		maxWidth: '228px',
+		height: '76px',
+		borderRadius: '1px'
+	}
+
+	return (
+		<div style={styles}></div>
+	);
+}
+
+export default Screen;
---
+++ src/util/interface.js
@@ -0,0 +1,80 @@
+const interfaceList = [
+	{
+		value: 'AC',
+		color: 'rgb(214, 214, 214)'
+	},
+	{
+		value: '+/-',
+		color: 'rgb(214, 214, 214)'
+	},
+	{
+		value: '%',
+		color: 'rgb(214, 214, 214)'
+	},
+	{
+		value: '/',
+		color: 'rgb(233, 151, 72)'
+	},
+	{
+		value: '7',
+		color: 'rgb(224, 224, 224)'
+	},
+	{
+		value: '8',
+		color: 'rgb(224, 224, 224)'
+	},
+	{
+		value: '9',
+		color: 'rgb(224, 224, 224)'
+	},
+	{
+		value: 'X',
+		color: 'rgb(233, 151, 72)'
+	},
+	{
+		value: '4',
+		color: 'rgb(224, 224, 224)'
+	},
+	{
+		value: '5',
+		color: 'rgb(224, 224, 224)'
+	},
+	{
+		value: '6',
+		color: 'rgb(224, 224, 224)'
+	},	
+	{
+		value: '-',
+		color: 'rgb(233, 151, 72)'
+	},	
+	{
+		value: '1',
+		color: 'rgb(224, 224, 224)'
+	},
+	{
+		value: '2',
+		color: 'rgb(224, 224, 224)'
+	},
+	{
+		value: '3',
+		color: 'rgb(224, 224, 224)'
+	},	
+	{
+		value: '+',
+		color: 'rgb(233, 151, 72)'
+	},	
+	{
+		value: '0',
+		color: 'rgb(224, 224, 224)'
+	},
+	{
+		value: '.',
+		color: 'rgb(224, 224, 224)'
+	},	
+	{
+		value: '=',
+		color: 'rgb(233, 151, 72)'
+	},	
+];
+
+export default interfaceList;
