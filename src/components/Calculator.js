67f39741a345c5f9fe1577e06a4d1b165f27daff
Dylan Smith <dylan_smartguy@hotmail.com>
2017-11-06 17:55:00 -0800

    started to build the numberpad and screen of calculator

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,15 +1,51 @@
 import React, { Component } from 'react';
 import Square from './Square';
+import Screen from './Screen';
+import interfaceList from '../util/interface';
+
+const styles = {
+	maxWidth: '228px' 
+}
 
 class Calculator extends Component {
 
 	constructor (props) {
 		super(props);
+
+		this.state = {
+			numberPad: []
+		}
+
+		this.drawCalc = this.drawCalc.bind(this);
+	}
+
+	drawCalc = (interfaceList) => {
+
+		const numberPad = [];
+
+		interfaceList.forEach( (interfaceObject, i) => {
+			console.log(interfaceObject);
+			numberPad.push(<Square key={i} squareValue={interfaceObject.value} color={interfaceObject.color} />)
+		});
+
+		this.setState({numberPad: numberPad});
+	}
+
+	componentWillMount () {
+		this.drawCalc(interfaceList);
 	}
 
 	render () {
 		return (
-			<Square />
+			<div>
+				<div>
+					<Screen />
+				</div>
+				<div style={styles}>
+					{this.state.numberPad}
+				</div>
+			</div>
+
 		);
 	}
 }
